f3b18c3706099731d84a035c7d4ffb4bc856f29c
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-09-19 15:32:29 +0200

    feat: update HomeComponent to display medals per country and integrate OlympicPieChartComponent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,83 +1,34 @@
 import { CommonModule } from "@angular/common";
-import { Component, inject, OnInit, ViewChild } from "@angular/core";
-import { ApexChart, ApexNonAxisChartSeries, ChartComponent, NgApexchartsModule, ApexLegend, ApexDataLabels, ApexTooltip } from "ng-apexcharts";
-import { Observable, of } from "rxjs";
+import { Component, inject, OnInit } from "@angular/core";
+import { Observable, of, tap } from "rxjs";
+import { OlympicPieChartComponent } from "~/components/olympic-pie-chart/olympic-pie-chart.component";
 import { Olympic } from "~/models/Olympic";
 import { OlympicService } from "~/services/olympic.service";
-
-interface ChartOptions {
-  series: ApexNonAxisChartSeries;
-  chart: ApexChart;
-  legend: ApexLegend;
-  labels: string[];
-  dataLabels: ApexDataLabels;
-  tooltip: ApexTooltip;
-}
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
-  imports: [CommonModule, NgApexchartsModule]
+  imports: [CommonModule, OlympicPieChartComponent]
 })
 export class HomeComponent implements OnInit {
-  @ViewChild("chart") chart!: ChartComponent;
-
-  public chartOptions: ChartOptions;
 
   public olympics$: Observable<Olympic[] | null> = of(null);
 
   private olympicService = inject(OlympicService);
 
-  constructor() {
-    this.chartOptions = {
-      series: [],
-      chart: {
-        type: "pie",
-        width: 400,
-        height: 400,
-      },
-      legend: {
-        show: false
-      },
-      labels: [],
-      dataLabels: {
-        enabled: false
-      },
-      tooltip: {
-        custom(options) {
-          console.log(options);
-          return `
-            <div>
-              <p class="country-name">
-                ${options.w.globals.labels[options.seriesIndex]}
-              </p>
-              <p class="medals-count">
-                🏅 ${options.series[options.seriesIndex]}
-              </p>
-            </div>
-          `;
-        },
-      }
-    };
-  }
+  public countJOS = 0;
+  public countCountries = 0;
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
 
-    this.olympics$.subscribe((olympics)=> {
-      if (!olympics) {
-        return;
-      }
-
-      const series = olympics.map(olympic => olympic.participations.reduce((sum, p) => sum + p.medalsCount, 0));
-
-      this.chartOptions.series = series;
-
-      const labels = olympics.map(olympic => olympic.country);
-
-      this.chartOptions.labels = labels;
-    });
-
+    this.olympics$.pipe(
+      tap((olympics) => {
+        this.countJOS = new Set(olympics?.map(o => o.participations.map(p => p.year)).flat()).size || 0;
+        this.countCountries = new Set(olympics?.map((o) => o.country)).size || 0;
+      })
+    ).subscribe();
   }
+
 }
