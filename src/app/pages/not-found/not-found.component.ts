0eadc76fd8150f6c62c6b4e3bf57c51b572289fa
Baptiste JULLION <contact@baptiste-jullion.dev>
2025-09-19 15:32:10 +0200

    refactor: simplify NotFoundComponent by removing unused imports and OnInit lifecycle hook

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-not-found",
   templateUrl: "./not-found.component.html",
   styleUrls: ["./not-found.component.scss"],
-
+  imports: [RouterLink]
 })
-export class NotFoundComponent implements OnInit {
-  ngOnInit(): void {
-
-    console.log("Page not found");
-
-  }
+export class NotFoundComponent  {
 
 }
